62681623c3bc2e4cf6c26fbbf5f1841557400542
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-30 11:21:36 +0300

    deployment to heroku

---
+++ instance/config.py
@@ -0,0 +1,3 @@
+MOVIE_API_KEY = '398af919e7f1cab0f969b7e8e97c4305'
+
+SECRET_KEY = 'kakwesi'
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export SECRET_KEY='kakwesi'
+export MOVIE_API_KEY='398af919e7f1cab0f969b7e8e97c4305'
+
+#
+python3.6 manage.py server
