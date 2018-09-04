6860af09ab9fd456c9cb5b8eba1bac3cd3ccff92
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-04 16:21:23 +0300

    Done, deployment

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,22 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
+
 
 def create_app(config_name):
 
@@ -15,6 +29,15 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # configure UploadSet
+    configure_uploads(app, photos)
+
+    #Configure mail extansion and initialise it to app access
+    mail.init_app(app)
+    simple.init_app(app)
 
     # Will add the views and forms
 
@@ -25,5 +48,8 @@ def create_app(config_name):
     # setting config
     from .requests import configure_request
     configure_request(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
     return app
--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 export SECRET_KEY='kakwesi'
 export MOVIE_API_KEY='398af919e7f1cab0f969b7e8e97c4305'
+export MAIL_USERNAME='raskwesi14@gmail.com'
+export MAIL_PASSWORD='Makonnen14'
 
 #
 python3.6 manage.py server
